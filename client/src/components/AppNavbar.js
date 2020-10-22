7c1e83ce56b837e5279ee818ef4665ec6e43845d
장재영 <[jaey5oung@naver.com]>
2020-10-22 17:05:27 +0900

    redux login 오류해결안됨

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react"
 import { Navbar, Container, NavbarToggler, Collapse, Nav } from "reactstrap"
 import { Link } from "react-router-dom"
+import LoginModal from "../components/auth/LoginModal"
 
 const AppNavbar = () => {
   return (
@@ -13,11 +14,7 @@ const AppNavbar = () => {
           <NavbarToggler />
           <Collapse isOpen={true} navbar>
             <Nav className="ml-auto d-flex justify-content-around" navbar>
-              {true ? (
-                <h1 className="text-white">authLink</h1>
-              ) : (
-                <h1 className="text-white">guestLink</h1>
-              )}
+              {false ? <h1 className="text-white">authLink</h1> : <LoginModal />}
             </Nav>
           </Collapse>
         </Container>
